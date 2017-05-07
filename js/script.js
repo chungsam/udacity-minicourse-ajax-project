310aa7566583f4dd4580e327150a63bca652344a
Sam Chung <chungsam@outlook.com>
2017-05-07 16:19:32 -0700

    Minor changes

--- js/script.js
+++ js/script.js
@@ -10,14 +10,15 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ' ' + cityStr;
+
     // load streetview
 
     function loadStreetView() {
         var streetviewUrl = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=";
 
-        var streetStr = $('#street').val();
-        var cityStr = $('#city').val();
-        var address = streetStr + ' ' + cityStr;
 
         var imgSrcUrl = streetviewUrl.concat(address);
 
@@ -38,13 +39,55 @@ function loadData() {
 
     // NYTimes 
     function loadNYTimes() {
+        $nytHeaderElem.text('Articles about ' + cityStr);
         var apiKey = '8654bfe302db4179bb820d3916338074';
-        var baseUrl = '';
+        var baseUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+        var location = $('#city').val();
+
+        var params = {
+            'api-key': apiKey,
+            'location': location
+        };
+
+        $.getJSON(baseUrl, params, function (res) {
+            var docs = res.response.docs;
+
+            docs.forEach(function (doc) {
+                var article;
+
+                // create headline element
+                var $headline = document.createElement('h2');
+                var $headlineLink = document.createElement('a');
+                $headlineLink.innerHTML = doc.headline.main;
+                $headlineLink.href = doc.web_url;
+                $headlineLink.target = '_blank';
+                $headline.appendChild($headlineLink);
+
+                // create snippet element
+                var $snippet = document.createElement('p');
+                $snippet.innerHTML = doc.snippet;
+                //a.src = doc.web
+
+                // create article element
+                article = document.createElement('li');
+                article.classList.add('article');
+
+                // append headline and snippet to article
+                article.appendChild($headline);
+                article.appendChild($snippet);
+
+                // finally, append the article to the list
+                $('#nytimes-articles').append(article);
+
+
+            })
+        });
     }
 
 
 
-
+    loadStreetView();
+    loadNYTimes();
 
     return false;
 };
